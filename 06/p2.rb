dfc24efd2a40e9c7ce49e1abeca06c071c503bd3
zlatsic <zlatan.sicanica@gmail.com>
2021-12-06 23:12:15 +0100

    06 done

--- 06/p2.rb
+++ 06/p2.rb
@@ -1,14 +1,18 @@
 input = open('06/input.txt').read
 initial_numbers = input.split(/,/).map(&:to_i)
 
-def process(period, days)
-  sum = 0
-  days.times do
-    sum += process(8, days - 1) if period.zero?
-    period = period.zero? ? 6 : period - 1
-    days -= 1
+$cache = {}
+
+def branch(days_left)
+  total = 1
+
+  periods_left = (days_left / 7 + 1)
+  return $cache[days_left] if $cache[days_left]
+
+  periods_left.times do |period|
+    total += branch(days_left - period * 7 - 9)
   end
-  sum + 1
+  $cache[days_left] = total
 end
 
-puts [3, 4, 3, 1, 2].map { |n| process(n, 80) }.sum
+puts initial_numbers.map { |n| branch(256 - n - 1) }.sum
